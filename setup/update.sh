4166f4bf6ea27e0994e2179838cb26896428ba28
coyuki <fu08yu20ko07@icloud.com>
2025-06-05 22:15:24 +0900

    fix

--- setup/update.sh
+++ setup/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if command -v brew >/dev/null 2>&1; then
-    brew update && brew upgrade
+    brew update && brew upgrade -y
 elif command -v apt >/dev/null 2>&1; then
     sudo apt update && apt upgrade -y
 elif command -v dnf >dev/null 2>&1; then
@@ -9,3 +9,4 @@ elif command -v pacman >dev/null 2>&1; then
     sudo pacman -Syyu
 else
     echo "sorry. don't support your os."
+fi
