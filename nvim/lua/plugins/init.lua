c6a25ca844a4a8f8a2f882820658b92760d07efa
coyuki <fu08yu20ko07@icloud.com>
2025-06-09 02:21:28 +0900

    add config for wez

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -64,7 +64,44 @@ return {
             })
         end
     },
-
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",  -- 最新バージョンを使用
+        cmd = "ToggleTerm", -- :ToggleTerm コマンドを有効にする
+        -- keys = {
+        --     -- F4キーでフローティングターミナルをトグル
+        --     { "<F4>",       "<cmd>ToggleTerm<cr>",                                                                     desc = "Toggle floating terminal" },
+        --     -- <leader>ft で現在の作業ディレクトリでフローティングターミナルを開く (LazyVimのデフォルトに似ています)
+        --     { "<leader>ft", function() require("toggleterm").toggle() end,                                             desc = "Terminal (Floating)" },
+        --     -- <leader>tc でcargo testをフローティングターミナルで実行
+        --     { "<leader>tc", function() require("toggleterm").toggle { cmd = "cargo test", dir = vim.fn.getcwd() } end, desc = "Run Cargo Test" },
+        --     -- <leader>rc でcargo runをフローティングターミナルで実行
+        --     { "<leader>rc", function() require("toggleterm").toggle { cmd = "cargo run", dir = vim.fn.getcwd() } end,  desc = "Run Cargo" },
+        -- },
+        opts = {
+            size = 80,
+            open_mapping = [[<c-t>]],
+            direction = "float", -- デフォルトをフローティングに設定
+            shade_filetypes = {},
+            hide_numbers = false,
+            insert_mappings = true,
+            terminal_mappings = true,
+            start_in_insert = true,
+            close_on_exit = true,
+            -- フローティングターミナルの外観設定
+            float_opts = {
+                border = "curved", -- 角が丸いボーダー
+                winblend = 5, -- 透明度
+                height = 30, -- 高さ
+                width = 80, -- 幅
+                row = nil, -- 中央配置
+                col = nil, -- 中央配置
+            },
+        },
+        config = function(_, opts)
+            require("toggleterm").setup(opts)
+        end,
+    },
     {
         "numToStr/Comment.nvim",
         event = { "BufReadPre", "BufNewFile" },
@@ -90,21 +127,21 @@ return {
         "akinsho/toggleterm.nvim",
         version = "*",
         config = function()
-          require("toggleterm").setup({
-            open_mapping = [[<c-\>]],  -- Ctrl+\ で開閉
-            direction = "float",       -- 浮動ウィンドウモードに設定
-            float_opts = {
-              border = "curved",       -- 角丸の枠（他に "single", "double", "shadow" など）
-              width = 80,
-              height = 20,
-              winblend = 10,            -- 透明度（0-100の数値で調整）
-            },
-            start_in_insert = true,    -- 開いたら自動で挿入モードに入る
-            close_on_exit = true,      -- シェル終了時に自動で閉じる
-          })
+            require("toggleterm").setup({
+                open_mapping = [[<c-\>]], -- Ctrl+\ で開閉
+                direction = "float",  -- 浮動ウィンドウモードに設定
+                float_opts = {
+                    border = "curved", -- 角丸の枠（他に "single", "double", "shadow" など）
+                    width = 80,
+                    height = 20,
+                    winblend = 10,  -- 透明度（0-100の数値で調整）
+                },
+                start_in_insert = true, -- 開いたら自動で挿入モードに入る
+                close_on_exit = true, -- シェル終了時に自動で閉じる
+            })
         end,
         keys = {
-          { "<c-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" },
+            { "<c-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" },
         },
     }
 }
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+
+return {
+    font = wezterm.font("0xProto"),
+    font_size = 10.0,
+
+    hide_tab_bar_if_only_one_tab = true,
+
+    color_scheme = "Catppuccin Macchiato",
+
+    initial_cols = 170,
+    initial_rows = 70,
+}
