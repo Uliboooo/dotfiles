74c0ce9978ecf8f17c5ace39c846b027c4f06c7c
coyuki <fu08yu20ko07@icloud.com>
2025-06-25 13:55:00 +0900

    wip

--- setup/fedora.sh
+++ setup/fedora.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 sudo dnf -y update
-sudo dnf -y install git tree sbcl vim neovim
+sudo dnf -y install git tree sbcl vim neovim glow
 
-git clone https://github.com/Uliboooo/dotfiles $HOME/
+git clone https://github.com/Uliboooo/dotfiles $HOME/dotfiles
 
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-DOT_DIR="$(dirname $SCRIPT_DIR)"
-ln -fs $DOT_DIR $HOME/.config
+ln -fs $HOME/dotfiles $HOME/.config
 
-source $HOME/.zshrc
+# source $HOME/.zshrc
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
