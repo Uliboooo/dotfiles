3db45290e8a60ec1ccf27781a2068414fb7bdf24
coyuki <fu08yu20ko07@icloud.com>
2025-06-25 14:53:50 +0900

    wip

--- setup/fedora.sh
+++ setup/fedora.sh
@@ -3,18 +3,28 @@
 sudo dnf -y update
 sudo dnf -y install git tree sbcl vim neovim glow
 
+[ -d "$HOME/dotfiles" ] && rm -rf "$HOME/dotfiles/"
 git clone https://github.com/Uliboooo/dotfiles $HOME/dotfiles
 
-ln -fs $HOME/dotfiles $HOME/.config
+config_list="git nano nvim starship wezterm zed zsh"
+dot_path="$HOME/dotfiles"
+config_path="$HOME/.config"
 
-# source $HOME/.zshrc
+for t in $config_list; do
+    ln -fs "$HOME/dotfiles/$t" "$HOME/.config/$t"
+done
+
+# ln -fs $HOME/dotfiles/git $config_path/git
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [ -d "$HOME/.linuxbrew" ]; then
+    eval "$($HOME/.linuxbrew/bin/brew shellenv)"
+elif [ -d "/home/linuxbrew/.linuxbrew" ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-brew install starship
+brew install starship zsh-autosuggestions zsh-syntax-highlighting
 
