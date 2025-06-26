eb9ca951fc6a243f49871b67eaa1475bb16d85f9
coyuki <fu08yu20ko07@icloud.com>
2025-06-26 13:56:04 +0900

    add keyconfig  as cargo fmt & chagen  to  as focus finderin nvim

--- nvim/lua/config/config.lua
+++ nvim/lua/config/config.lua
@@ -33,10 +33,12 @@ vim.g.rustaceanvim = {
     },
 }
 
-vim.keymap.set("n", "<leader>f", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>p", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
+
+vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.format({ async = false })<CR>', { noremap = true, silent = true })
 
 vim.api.nvim_create_user_command(
   "W",           -- コマンド名
--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = "\\"
 
-vim.keymap.set("n", "<leader>f", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP Rename" })
