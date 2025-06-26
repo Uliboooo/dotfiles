74154cc6c306b2d5885fb02e9f0ccac4e10fbe2e
coyuki <fu08yu20ko07@icloud.com>
2025-06-26 13:48:54 +0900

    enable rustowl in nvim

--- nvim/lua/plugins/rust.lua
+++ nvim/lua/plugins/rust.lua
@@ -25,11 +25,11 @@ return {
         ft = { 'rust' },
     },
 
-    -- {
-    --   'cordx56/rustowl',
-    --   version = '*', -- Latest stable version
-    --   build = 'cargo binstall rustowl',
-    --   lazy = false, -- This plugin is already lazy
-    --   opts = {},
-    -- },
+    {
+      'cordx56/rustowl',
+      version = '*', -- Latest stable version
+      build = 'cargo binstall rustowl',
+      lazy = false, -- This plugin is already lazy
+      opts = {},
+    },
 }
