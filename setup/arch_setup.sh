a81e3765d415540c1cd904bfee4612d79755f7c2
uliboooo <uliboulibo@gmail.com>
2025-06-28 17:49:44 +0900

    update arch_setup

--- setup/arch_setup.sh
+++ setup/arch_setup.sh
@@ -6,19 +6,20 @@ if ! type "zsh" > /dev/null 2>&1; then
     sudo pacman -S zsh --noconfirm
     echo "installing zsh..."
 fi
-if type "git" > /dev/null 2>&1; then
+if ! type "git" > /dev/null 2>&1; then
     sudo pacman -S git --noconfirm
 fi
 
-echo "what's your name?"
-read name
-current_shelll=$(getent passwd "$name" | cut -d: -f7 | rev | cut -d/ -f1 | rev)
-if [ "$current_shelll" != "zsh" ]; then
-    chsh -s $(which zsh) "$name"
+# echo "what's your name?"
+current_shell=$(getent passwd $USER | cut -d: -f7 | rev | cut -d/ -f1 | rev)
+if [ "$current_shell" != "zsh" ]; then
+    chsh -s "$(which zsh)" "$USER"
 fi
 
 if [ -d "$HOME/dotfiles" ]; then
-    rm -rf "$HOME/dotfiles"
+    BACKUP_DIR="$HOME/dotfiles_old_$(date +%Y%m%d-%H%M%S)"
+    echo "Found existing $HOME/dotfiles. Backing it up to $BACKUP_DIR"
+    mv "$HOME/dotfiles" "$BACKUP_DIR"
 fi
 
 git clone https://github.com/Uliboooo/dotfiles $HOME/dotfiles
