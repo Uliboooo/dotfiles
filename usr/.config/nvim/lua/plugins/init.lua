4fef8bf3e62d08f0e1baaecd3884ea64aebff750
uliboooo <uliboulibo@gmail.com>
2025-06-30 14:12:50 +0900

    add lualikne to nv

--- usr/.config/nvim/lua/plugins/init.lua
+++ usr/.config/nvim/lua/plugins/init.lua
@@ -138,4 +138,38 @@ return {
         }
       end,
     },
+
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function()
+        require('lualine').setup {
+          options = {
+            icons_enabled = true,
+            theme = 'auto', -- 'auto', 'powerline_dark', 'nord', など好きなテーマを選択
+            component_separators = { left = '', right = ''}, -- アイコンを使用する場合
+            section_separators = { left = '', right = ''}, -- アイコンを使用する場合
+            -- その他のオプション
+          },
+          sections = {
+            lualine_a = {'mode'},
+            lualine_b = {'branch', 'diff', 'diagnostics'},
+            lualine_c = {'filename'},
+            lualine_x = {'filesize', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+          },
+          inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+          },
+          tabline = {},
+          extensions = {}
+        }
+      end
+    }
 }
