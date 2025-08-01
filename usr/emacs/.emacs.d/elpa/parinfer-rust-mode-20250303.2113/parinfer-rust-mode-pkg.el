a6846632b14807759264ef450d0007b1a306ae44
uliboooo <uliboulibo@gmail.com>
2025-08-01 09:00:12 +0900

    wip

---
+++ usr/emacs/.emacs.d/elpa/parinfer-rust-mode-20250303.2113/parinfer-rust-mode-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "parinfer-rust-mode" "20250303.2113"
+  "An interface for the parinfer-rust library."
+  '((emacs         "26.1")
+    (track-changes "1.1"))
+  :url "https://github.com/justinbarclay/parinfer-rust-mode"
+  :commit "d5578ec0b2e907cb1a2600057f524135c4553050"
+  :revdesc "d5578ec0b2e9"
+  :keywords '("lisp" "tools")
+  :authors '(("Justin Barclay" . "justinbarclay@gmail.com"))
+  :maintainers '(("Justin Barclay" . "justinbarclay@gmail.com")))
--- usr/emacs/.emacs.d/init.el
+++ usr/emacs/.emacs.d/init.el
@@ -1,37 +1,110 @@
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(message "✅ init.el loaded successfully.")
+;; -------------------------------
+;; パッケージ管理の初期設定
+;; -------------------------------
+(require 'package)
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
 
-;; 使用するCommon Lisp処理系を指定（SBCLの場合）
-(setq inferior-lisp-program "sbcl")
+(package-initialize)
 
-;; SLIMEの設定を必要に応じて追加
-(require 'slime)
-(slime-setup '(slime-fancy))  ;; fancy は補完やデバッガ機能を有効化
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(xterm-mouse-mode 1)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-(use-package yasnippet
-  :ensure t
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+;; -------------------------------
+;; Vim キーバインド (evil-mode)
+;; -------------------------------
+(use-package evil
   :config
-  (yas-global-mode 1))
+  (evil-mode 1)
+  (setq evil-want-C-u-scroll t))
 
-;; rust
-(add-to-list 'exec-path (expand-file-name "~/.cargo/bin//rust-analyzer"))
-(add-to-list 'exec-path (expand-file-name "~/.cargo/bin"))
+;; -------------------------------
+;; SLIME + SBCL 設定（Common Lisp）
+;; -------------------------------
+(setq inferior-lisp-program "/usr/bin/sbcl") ;; ← SBCL のパスを環境に合わせて修正
 
-(use-package rust-mode
-             :ensure t
-             :custom rust-format-on-save t)
+(use-package slime
+  :config
+  (setq slime-contribs '(slime-fancy))
+  (slime-setup))
 
-(use-package cargo
-             :ensure t
-             :hook (rust-mode . cargo-minor-mode))
+;; -------------------------------
+;; Parinfer（括弧＋インデント支援）
+;; -------------------------------
 
-(use-package lsp-mode
-             :ensure t
-             :init (yas-global-mode)
-             :hook (rust-mode . lsp)
-             :bind ("C-c h" . hsp-desribe-thing-at-point)
-             :custom (lsp-rust-server 'rust-analyzer))
-(use-package lsp-ui
-             :ensure t)
+;; old
+;; (use-package parinfer
+;;   :hook ((lisp-mode       . parinfer-mode)
+;;          (slime-repl-mode . parinfer-mode))
+;;   :config
+;;   (setq parinfer-extensions
+;;         '(defaults
+;;           pretty-parens
+;;           smart-tab
+;;           smart-yank)))
+
+(use-package parinfer-rust-mode
+  :hook ((lisp-mode . parinfer-rust-mode)
+         (emacs-lisp-mode . parinfer-rust-mode))
+  :config
+  (setq parinfer-rust-auto-download t))
+
+;; -------------------------------
+;; paredit（括弧の構造編集）
+;; -------------------------------
+(use-package paredit
+  :hook ((lisp-mode       . enable-paredit-mode)
+         (slime-repl-mode . enable-paredit-mode)))
+
+;; -------------------------------
+;; lispy（超効率的なS式編集）
+;; -------------------------------
+(use-package lispy
+  :hook ((lisp-mode       . lispy-mode)
+         (slime-repl-mode . lispy-mode)))
+
+;; -------------------------------
+;; 保存時に自動整形（インデント）
+;; -------------------------------
+(defun my-lisp-auto-indent ()
+  "保存時にバッファ全体をインデント"
+  (when (or (eq major-mode 'lisp-mode)
+            (eq major-mode 'slime-repl-mode))
+    (indent-region (point-min) (point-max))))
+
+(add-hook 'before-save-hook #'my-lisp-auto-indent)
+
+;; -------------------------------
+;; 補完・括弧カラー
+;; -------------------------------
+(use-package company
+  :hook (after-init . global-company-mode))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; -------------------------------
+;; UI をスッキリ
+;; -------------------------------
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-screen t)
+(load-theme 'tango-dark t)
+
+;; -------------------------------
+;; カスタムファイルを別に保存
+;; -------------------------------
+(setq custom-file "~/.emacs.d/custom.el")
+(when (file-exists-p custom-file)
+  (load custom-file))
 
