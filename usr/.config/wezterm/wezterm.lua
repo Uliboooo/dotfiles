859fbc3f82427d17b5207fda2b6203dbca7b7f2a
uliboooo <uliboulibo@gmail.com>
2025-08-13 17:51:52 +0900

    feat: Update wezterm config to use system tmux and improve font configuration

--- usr/.config/wezterm/wezterm.lua
+++ usr/.config/wezterm/wezterm.lua
@@ -1,35 +1,40 @@
-local wezterm = require 'wezterm'
-local config = {};
+local wezterm = require("wezterm")
+
+local tm
+if wezterm.target_triple:match("darwin") then
+  tm = "/opt/homebrew/bin/tmux"
+else
+  tm = "tmux"
+end
+
 return {
-    audible_bell = "SystemBeep",
+  audible_bell = "SystemBeep",
 
-    font = wezterm.font_with_fallback{
-        "0xProto Nerd Font Mono",
-        "0xProto",
-        "Cica",
-    },
+  font = wezterm.font_with_fallback({
+    "0xProto Nerd Font Mono",
+    "0xProto",
+    "Cica",
+  }),
 
-    font_size = 10.5,
+  font_size = 10.5,
 
-    harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" },
+  harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" },
 
-    hide_tab_bar_if_only_one_tab = true,
+  hide_tab_bar_if_only_one_tab = true,
 
-    color_scheme = "Catppuccin Macchiato",
+  color_scheme = "Catppuccin Macchiato",
 
-    initial_cols = 170,
-    initial_rows = 70,
+  initial_cols = 170,
+  initial_rows = 70,
 
-    default_cursor_style = "BlinkingBar",
-    cursor_blink_rate = 600,
+  default_cursor_style = "BlinkingBar",
+  cursor_blink_rate = 600,
 
-    default_cwd = "$HOME",
+  default_cwd = "$HOME",
+  default_prog = { tm },
 
-    exit_behavior = "Close",
-    keys = {
-        { key = 't', mods = 'SUPER', action = wezterm.action.SpawnCommandInNewTab { cwd = '~' } },
-    },
-
-    default_prog = {"tmux"},
+  exit_behavior = "Close",
+  keys = {
+    { key = "t", mods = "SUPER", action = wezterm.action.SpawnCommandInNewTab({ cwd = "~" }) },
+  },
 }
-
