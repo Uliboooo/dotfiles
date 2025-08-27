89b90222df996c34209fff24fab3c0da6dee5e8b
uliboooo <uliboulibo@gmail.com>
2025-08-27 14:29:24 +0900

    fix: Corrected typo in redo command and keymap setting

--- usr/.config/nvim/lua/config/keymap.lua
+++ usr/.config/nvim/lua/config/keymap.lua
@@ -4,13 +4,13 @@ vim.keymap.set("n", "<Leader>a", vim.lsp.buf.code_action, { desc = "LSP Code Act
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "LSP Rename" })
 vim.keymap.set("n", "<leader>n", ":nohlsearch<CR>", { silent = true })
-vim,keymap.set("n", "<Leader>U", vim.cmd.redo, { desc = "Redo" })
+vim.keymap.set("n", "<leader>U", vim.cmd.redu, { desc = "redo" })
 
-vim.keymap.set({"n", "i", "v", "x", "s"}, "<C-c>", function()
+vim.keymap.set({ "n", "i", "v", "x", "s" }, "<C-c>", function()
   require("Comment.api").toggle.linewise.current()
 end, { desc = "toggle cooment for ", noremap = true })
 
-vim.keymap.set({"n", "v", "x", "s"}, "<Leader>gc", function()
+vim.keymap.set({ "n", "v", "x", "s" }, "<Leader>gc", function()
   require("Comment.api").toggle.linewise.current()
 end, { desc = "toggle cooment for ", noremap = true })
 
@@ -49,4 +49,3 @@ end, { desc = "symbol search" })
 vim.keymap.set("n", "gr", function()
   require("snacks").picker.lsp_references()
 end, { desc = "lsp_references" })
-
