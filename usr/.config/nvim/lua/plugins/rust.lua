2d4e8adf3788224a32c786def385ff729f96eefe
uliboooo <uliboulibo@gmail.com>
2025-09-02 22:09:09 +0900

    refactor: remove redundant Neovim configuration

--- usr/.config/nvim/lua/plugins/rust.lua
+++ usr/.config/nvim/lua/plugins/rust.lua
@@ -1,66 +1,57 @@
 -- rust.lua
 
 return {
-  {
-    "saecki/crates.nvim",
-    tag = "stable",
-    config = function()
-      require("crates").setup()
-    end,
-  },
-  --
-  -- {
-  --     "williamboman/mason.nvim",
-  --     opts = {
-  --         ensure_installed = {
-  --             "codelldb",
-  --         },
-  --     },
-  -- },
+	{
+		"saecki/crates.nvim",
+		tag = "stable",
+		config = function()
+			require("crates").setup()
+		end,
+	},
+	--
+	-- {
+	--     "williamboman/mason.nvim",
+	--     opts = {
+	--         ensure_installed = {
+	--             "codelldb",
+	--         },
+	--     },
+	-- },
 
-  {
-    "mrcjkb/rustaceanvim",
-    version = "^6",
-    lazy = false,
-    ft = { "rust" },
-    init = function()
-      vim.g.rustaceanvim = {
-        server = {
-          on_attach = function(_, bufnr)
-            local buf_map = function(lhs, rhs)
-              vim.keymap.set("n", lhs, rhs, { buffer = bufnr })
-            end
-            buf_map("gd", vim.lsp.buf.definition)
-            buf_map("K", vim.lsp.buf.hover)
-          end,
-        },
-      }
-    end,
-  },
-  -- {
-  --   'cordx56/rustowl',
-  --   version = '*', -- Latest stable version
-  --   build = 'cargo binstall rustowl',
-  --   lazy = false, -- This plugin is already lazy
-  --   opts = {
-  --     auto_enable = true,
-  --     idle_time = 1000,
-  --   },
-  -- }
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^6",
+		lazy = false,
+		ft = { "rust" },
+		init = function()
+			vim.g.rustaceanvim = {
+				server = {
+					path = "$HOME/.cargo/bin//rust-analyzer",
+					on_attach = function(_, bufnr)
+						local buf_map = function(lhs, rhs)
+							vim.keymap.set("n", lhs, rhs, { buffer = bufnr })
+						end
+						buf_map("gd", vim.lsp.buf.definition)
+						buf_map("K", vim.lsp.buf.hover)
+					end,
+				},
+			}
+		end,
+	},
 
-  {
-    "cordx56/rustowl",
-    version = "*", -- Latest stable version
-    build = "cargo binstall rustowl",
-    lazy = false, -- This plugin is already lazy
-    opts = {
-      client = {
-        on_attach = function(_, buffer)
-          vim.keymap.set("n", "<leader>o", function()
-            require("rustowl").toggle(buffer)
-          end, { buffer = buffer, desc = "Toggle RustOwl" })
-        end,
-      },
-    },
-  },
+	{
+		"cordx56/rustowl",
+		version = "*", -- Latest stable version
+		build = "cargo binstall rustowl",
+		lazy = false, -- This plugin is already lazy
+		opts = {
+			client = {
+				on_attach = function(_, buffer)
+					vim.keymap.set("n", "<leader>o", function()
+						require("rustowl").toggle(buffer)
+					end, { buffer = buffer, desc = "Toggle RustOwl" })
+				end,
+			},
+		},
+	},
 }
