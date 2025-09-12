6df91b9a8b8448fd8b61619e5fdb719696f822a2
uliboooo <uliboulibo@gmail.com>
2025-09-13 04:56:26 +0900

    remove realtime markdown preview in nvim

--- usr/.config/nvim/lua/plugins/md.lua
+++ usr/.config/nvim/lua/plugins/md.lua
@@ -17,15 +17,15 @@ return {
   },
 
   -- Preview in browser
-  {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    ft = { "markdown" },
-    cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
-    init = function()
-      vim.g.mkdp_auto_start = 0 -- 自動プレビューしない
-    end,
-  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   build = "cd app && npm install",
+  --   ft = { "markdown" },
+  --   cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+  --   init = function()
+  --     vim.g.mkdp_auto_start = 0 -- 自動プレビューしない
+  --   end,
+  -- },
 
   -- Writing focus
   {
