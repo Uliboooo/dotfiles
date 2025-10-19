eb3718ca287bafad74bbe0d60a33c507717d78c2
uliboooo <uliboulibo@gmail.com>
2025-10-19 23:30:16 +0900

    feat(config): add git credential helper and nvim markdown parsers

--- usr/.config/nvim/lua/plugins/treesitter.lua
+++ usr/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   opts = {
-    ensure_installed = { "lua", "ocaml", "c", "cpp" },
+    ensure_installed = { "lua", "ocaml", "c", "cpp", "markdown", "markdown_inline" },
     sync_install = true,
   },
 }
