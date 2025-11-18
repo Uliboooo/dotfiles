738c72b5875f0e5d53ba8f3cf1f1cf110699071f
uliboooo <uliboulibo@gmail.com>
2025-11-18 15:45:49 +0900

    chore: Disable Neovim LLM plugin and enable markdown tools

--- usr/.config/nvim/lua/plugins/md.lua
+++ usr/.config/nvim/lua/plugins/md.lua
@@ -1,49 +1,49 @@
 return {
   -- Markdown syntax & folding
-  {
-    "preservim/vim-markdown",
-    dependencies = { "godlygeek/tabular" },
-    ft = { "markdown" },
-    config = function()
-      vim.g.vim_markdown_folding_disabled = 1
-      vim.g.vim_markdown_conceal = 0
-    end,
-  },
-
-  -- Table formatting
-  {
-    "dhruvasagar/vim-table-mode",
-    ft = { "markdown" },
-    cmd = { "TableModeToggle" },
-  },
-
-  -- Preview in browser
   -- {
-  --   "iamcco/markdown-preview.nvim",
-  --   build = "cd app && npm install",
+  --   "preservim/vim-markdown",
+  --   dependencies = { "godlygeek/tabular" },
   --   ft = { "markdown" },
-  --   cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
-  --   init = function()
-  --     vim.g.mkdp_auto_start = 0 -- 自動プレビューしない
+  --   config = function()
+  --     vim.g.vim_markdown_folding_disabled = 1
+  --     vim.g.vim_markdown_conceal = 0
   --   end,
   -- },
-
-  -- Writing focus
-  {
-    "junegunn/goyo.vim",
-    cmd = { "Goyo" },
-  },
-  {
-    "junegunn/limelight.vim",
-    cmd = { "Limelight" },
-  },
-
-  -- Better wrapping for prose
-  {
-    "preservim/vim-pencil",
-    ft = { "markdown" },
-    config = function()
-      vim.cmd("PencilSoft")
-    end,
-  },
+  --
+  -- -- Table formatting
+  -- {
+  --   "dhruvasagar/vim-table-mode",
+  --   ft = { "markdown" },
+  --   cmd = { "TableModeToggle" },
+  -- },
+  --
+  -- -- Preview in browser
+  -- -- {
+  -- --   "iamcco/markdown-preview.nvim",
+  -- --   build = "cd app && npm install",
+  -- --   ft = { "markdown" },
+  -- --   cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+  -- --   init = function()
+  -- --     vim.g.mkdp_auto_start = 0 -- 自動プレビューしない
+  -- --   end,
+  -- -- },
+  --
+  -- -- Writing focus
+  -- {
+  --   "junegunn/goyo.vim",
+  --   cmd = { "Goyo" },
+  -- },
+  -- {
+  --   "junegunn/limelight.vim",
+  --   cmd = { "Limelight" },
+  -- },
+  --
+  -- -- Better wrapping for prose
+  -- {
+  --   "preservim/vim-pencil",
+  --   ft = { "markdown" },
+  --   config = function()
+  --     vim.cmd("PencilSoft")
+  --   end,
+  -- },
 }
