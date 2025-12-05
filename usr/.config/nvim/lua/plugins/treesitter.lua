ac6d15779cf2a06332b6e87a690333b15b23c2b2
uliboooo <uliboulibo@gmail.com>
2025-12-05 09:19:33 +0900

    feat: configure editor plugins and terminal appearance

--- usr/.config/nvim/lua/plugins/treesitter.lua
+++ usr/.config/nvim/lua/plugins/treesitter.lua
@@ -3,16 +3,53 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate | TSInstall go",
     opts = {
-      ensure_installed = { "lua", "ocaml", "c", "cpp", "markdown", "markdown_inline", "go", "javascript", "typescript" },
+      ensure_installed = {
+        "lua",
+        "ocaml",
+        "c",
+        "cpp",
+        "markdown",
+        "markdown_inline",
+        "go",
+        "javascript",
+        "typescript",
+        "commonlisp",
+      },
       sync_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
+      config = function(_, opts)
+        require("nvim-treesitter.config").setup(opts)
+      end,
     },
   },
 
   {
     "Olical/conjure",
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    config = function()
+      local rainbow_delimiters = require("rainbow-delimiters")
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+        },
+        highlight = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterViolet",
+          "RainbowDelimiterCyan",
+        },
+      }
+    end,
   },
 }
