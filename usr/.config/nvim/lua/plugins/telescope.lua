55d19b18f3debaca6c98b8a6cb2063d066822617
uliboooo <uliboulibo@gmail.com>
2025-12-11 21:05:47 +0900

    feat: Add telescope fzf-native extension and update keymaps

--- usr/.config/nvim/lua/plugins/telescope.lua
+++ usr/.config/nvim/lua/plugins/telescope.lua
@@ -1,43 +1,62 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "v0.2.0",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local btin = require("telescope.builtin")
-
-    telescope.setup({
-      defaults = {
-        hidden = false,
-        vimgrep_arguments = {
-          "rg",
-          "--color=never",
-          "--no-heading",
-          "--with-filename",
-          "--line-number",
-          "--column",
-          "--smart-case",
-          "--hidden",
-        },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "v0.2.0",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
       },
-      pickers = {
-        find_files = {
-          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-        },
-      },
-    })
-    -- Keymaps
-    vim.keymap.set("n", "<Leader>f", btin.find_files, { desc = "telescope find files" })
-    vim.keymap.set("n", "<Leader>/", btin.live_grep, { desc = "telescope live grep" })
-    vim.keymap.set("n", "<Leader>b", btin.buffers, { desc = "telescope buffers" })
-    vim.keymap.set("n", "<Leader>q", btin.quickfix, { desc = "telescope quickfix" })
-    vim.keymap.set("n", "<Leader>s", btin.lsp_document_symbols, { desc = "telescope lsp doc symbols" })
-    vim.keymap.set("n", "<Leader>d", btin.diagnostics, { desc = "telescope diagnostics" })
-    vim.keymap.set("n", "<Leader>P", btin.commands, { desc = "telescope vim cmd list" })
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local btin = require("telescope.builtin")
 
-    vim.keymap.set("n", "gr", btin.lsp_references, { desc = "telescope lsp references" })
-    vim.keymap.set("n", "gd", btin.lsp_definitions, { desc = "telescope lsp definitions" })
-    vim.keymap.set("n", "gt", btin.lsp_type_definitions, { desc = "telescope lsp type def" })
-  end,
+      telescope.setup({
+        defaults = {
+          hidden = false,
+          vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+          },
+        },
+        pickers = {
+          find_files = {
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+          },
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case",
+          },
+        },
+      })
+
+      require("telescope").load_extension("fzf")
+
+      -- Keymaps
+      vim.keymap.set("n", "<Leader>f", btin.find_files, { desc = "telescope find files" })
+      vim.keymap.set("n", "<Leader>/", btin.live_grep, { desc = "telescope live grep" })
+      vim.keymap.set("n", "<Leader>b", btin.buffers, { desc = "telescope buffers" })
+      vim.keymap.set("n", "<Leader>q", btin.quickfix, { desc = "telescope quickfix" })
+      vim.keymap.set("n", "<Leader>s", btin.lsp_document_symbols, { desc = "telescope lsp doc symbols" })
+      vim.keymap.set("n", "<Leader>d", btin.diagnostics, { desc = "telescope diagnostics" })
+      vim.keymap.set("n", "<Leader>P", btin.commands, { desc = "telescope vim cmd list" })
+
+      vim.keymap.set("n", "gr", btin.lsp_references, { desc = "telescope lsp references" })
+      vim.keymap.set("n", "gd", btin.lsp_definitions, { desc = "telescope lsp definitions" })
+      vim.keymap.set("n", "gt", btin.lsp_type_definitions, { desc = "telescope lsp type def" })
+    end,
+  },
 }
