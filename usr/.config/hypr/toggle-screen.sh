480985306b57722e4c1435f1d6c288a08fdc7174
uliboooo <uliboulibo@gmail.com>
2025-12-21 12:00:58 +0900

    fix toggle-screen.sh error

--- usr/.config/hypr/toggle-screen.sh
+++ usr/.config/hypr/toggle-screen.sh
@@ -3,16 +3,10 @@
 INTERNAL_MONITOR="eDP-1"
 EXTERNAL_MONITOR="DP-1"
 
-IS_INTERNAL_DISABLED=$(hyprctl monitors | grep "Monitor $INTERNAL_MONITOR" | grep "enabled: no" | wc -l)
+if ! hyprctl monitors | grep -q "Monitor $INTERNAL_MONITOR"; then
+  hyprctl reload
 
-if [ "$IS_INTERNAL_DISABLED" -ge 1 ]; then
-  echo "switching to dual mode"
-
-  hyprctl keyword monitor "$INTERNAL_MONITOR, preferred, auto, 1"
-  hyprctl keyword monitor "$EXTERNAL_MONITOR, preferred, auto, 1"
 else
-  echo "switching to external only"
-
   hyprctl keyword monitor "$INTERNAL_MONITOR, disable"
   hyprctl keyword monitor "$EXTERNAL_MONITOR, preferred, 0x0, 1"
 fi
