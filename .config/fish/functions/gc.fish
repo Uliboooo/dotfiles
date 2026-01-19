016861196afd51a2b5b83a533aab24e0b1b8fb0a
uliboooo <uliboulibo@gmail.com>
2026-01-19 15:01:24 +0900

    remove fish

--- .config/fish/functions/gc.fish
+++
@@ -1,4 +0,0 @@
-function gc --description 'Stage all changes and commit'
-    git add .
-    git commit -m "$argv[1]"
-end
--- .config/fish/functions/touch-p.fish
+++
@@ -1,4 +0,0 @@
-function touch-p --description 'Create directory and touch file'
-    mkdir -p (dirname $argv[1])
-    and touch $argv[1]
-end
