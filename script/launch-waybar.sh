db54198378ee924ba33e836bd30df82b7c24877c
uliboooo <uliboulibo@gmail.com>
2026-02-21 00:55:22 +0900

    update waybar launch methods

--- script/launch-waybar.sh
+++ script/launch-waybar.sh
@@ -4,4 +4,10 @@ killall waybar || true
 pkill waybar || true
 sleep 0.5
 
-waybar -c $HOME/dotfiles/.config/waybar/config.niri.jsonc -s $HOME/dotfiles/.config/waybar/style.css &
+if pgrep -x Hyprland > /dev/null; then
+    waybar -c $HOME/dotfiles/.config/waybar/config.hypr.jsonc -s $HOME/dotfiles/.config/waybar/style.css &
+    echo "spawn waybay for hyprland"
+elif pgrep -x niri > /dev/null; then
+    waybar -c $HOME/dotfiles/.config/waybar/config.niri.jsonc -s $HOME/dotfiles/.config/waybar/style.css &
+    echo "spawn waybar for niri"
+fi
