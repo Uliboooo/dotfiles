d17ce8048e4287dae58660c283207ad7d7434d66
uliboooo <uliboulibo@gmail.com>
2026-03-08 18:36:06 +0900

    fix wallpaper controls
    - mv safe_wallpaper to script/
    - add rev mode in wallpaper changer

--- .config/hypr/safe_wallpaper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WALLPAPER="~/Pictures/safe_wallpaper/snow.jpg"
-hyprctl hyprpaper unload all
-hyprctl hyprpaper preload "$WALLPAPER"
-hyprctl hyprpaper wallpaper ",$WALLPAPER"
--- script/cycle_wallpaper.sh
+++ script/cycle_wallpaper.sh
@@ -13,24 +13,39 @@ get_first_path() {
 # $2: source files list
 # return: next line from pattern
 get_next_path() {
-  MATCHED="$(echo "$2" | grep "$1" -A 1)"
+  REV_MODE=$3
+  if (( $REV_MODE == 0 )); then
+    MATCHED="$(echo "$2" | grep "$1" -A 1)"
+  else
+    MATCHED="$(echo "$2" | grep "$1" -B 1)"
+  fi
   MATCHED_LINES=$(echo "$MATCHED" | wc -l)
 
   if (( MATCHED_LINES != 2 )); then
     echo "$2" | head -1
   else
-    echo "$MATCHED" | tail -1
+    if (( $REV_MODE == 0)); then
+      echo "$MATCHED" | tail -1
+    else
+      echo "$MATCHED" | head -1
+    fi
   fi
 }
 
 WALLPAPER_DIR="$HOME/Pictures/wallpapers/"
 CURRENT_PAPER_PATH="$HOME/dotfiles/.config/hypr/env/CURRENT_PAPER"
+# 0 is false, and default is false
+if [ -z "$1" ]; then
+  REV_MODE=0
+else
+  REV_MODE=1
+fi
 
 if [ -s $CURRENT_PAPER_PATH ]; then
   CURRENT_PAPER=$(cat $CURRENT_PAPER_PATH)
   if [ -f $CURRENT_PAPER_PATH ]; then
     LIST=$(get_all $WALLPAPER_DIR)
-    NEXT=$(get_next_path "$CURRENT_PAPER" "$LIST")
+    NEXT=$(get_next_path "$CURRENT_PAPER" "$LIST" "$REV_MODE")
   else
     NEXT=$(get_first_path $WALLPAPER_DIR)
   fi
