7275480d8e7815678dc3990ddd52582d5f21addc
uliboooo <uliboulibo@gmail.com>
2026-03-23 02:13:35 +0900

    update design of fuzzel & revert to toggleterm

--- .config/nvim/lua/plugins/term.lua
+++ .config/nvim/lua/plugins/term.lua
@@ -1,24 +1,39 @@
 return {
   {
-    "ruicsh/termite.nvim",
-    opts = {
-      width = 0.6,
-      heiht = 0.6,
-      position = "bottom",
-      border = "light",
-      shell = nil,
-      start_insert = true,
-      winbar = true,
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        direction = "horizontal",
+        size = 20,
+      })
 
-      keymaps = {
-        toggle = "<C-\\>",
-        create = "<C-t>",
-        next = "<C-n>",
-        prev = "<C-p>",
-        focus_editor = "<C-e>",
-        maximize = "<C-f>",
-        close = "<C-q>",
-      },
-    },
+      local term_state = 0
+
+      vim.keymap.set({ "n", "t" }, [[<C-\>]], function()
+        if term_state == 0 then
+          vim.cmd("ToggleTerm")
+          vim.cmd("resize 20")
+          term_state = 1
+        elseif term_state == 1 then
+          vim.cmd("resize " .. (vim.o.lines - 5))
+          term_state = 2
+        else
+          vim.cmd("ToggleTerm")
+          term_state = 0
+        end
+      end)
+
+      -- Esc Esc で強制 off
+      vim.keymap.set("t", "<Esc><Esc>", function()
+        vim.cmd("ToggleTerm")
+        term_state = 0
+      end)
+
+      -- 手動で閉じた場合もリセット
+      vim.api.nvim_create_autocmd("TermClose", {
+        callback = function() term_state = 0 end,
+      })
+    end,
   },
 }
