53669e7b0a9cfb26aec696b4aac0bde49a4813df
uliboooo <uliboulibo@gmail.com>
2026-04-25 15:53:26 +0900

    fix tlp-waybar-status

--- script/tlp-waybar-status.sh
+++ script/tlp-waybar-status.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-tlp-stat -s | grep 'Power profile' | awk '{print $4}' |
+tlp-stat -s | grep -Ei 'Power profile|TLP Profile' | awk '{print $4}' |
   awk -F'/' '{m=$1=="performance"?"PRF":$1=="balanced"?"BAL":$1=="powersave"?"SAV":$1; print NF==2?m"/"$2:m}'
