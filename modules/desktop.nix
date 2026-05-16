03fcbb49df3fda939cde8e17bc6fea01240d9374
uliboooo <uliboulibo@gmail.com>
2026-05-16 23:39:34 +0900

    nix: update

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -24,12 +24,39 @@
 
   # polkit
   security.polkit.enable = true;
+  # gnome-keyring を NixOS サービスとして有効化
+  services.gnome.gnome-keyring.enable = true;
+  # ログイン時に keyring を PAM 連携で解錠
+  security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.gdm.enableGnomeKeyring = true;
+  security.pam.services.gdm-password.enableGnomeKeyring = true;
 
   # GDM (GUI ログイン) を使う
   services.xserver.enable = true;
   services.displayManager.gdm.enable = true;
   services.displayManager.defaultSession = "hyprland";
   services.greetd.enable = false;
+
+  # 省電力管理 (TLP)
+  services.tlp.enable = true;
+  # TLP と競合しやすいため無効化
+  services.power-profiles-daemon.enable = false;
+
+  services.flatpak = {
+    enable = true;
+
+    remotes = [
+      {
+        name = "flathub";
+        location = "https://flathub.org/repo/flathub.flatpakrepo";
+      }
+    ];
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
 
   environment.systemPackages = with pkgs; [
     # Hyprland 周辺ユーティリティ
@@ -38,8 +65,11 @@
     hyprpaper
     hyprlock
     hypridle
+    hyprpicker
+    hyprshot
     wl-clipboard
     swaynotificationcenter
     kitty
+    cliphist
   ];
 }
