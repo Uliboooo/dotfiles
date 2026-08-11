e9da9b2ef860caf7bf558a70e331a250efc6a842
seli <seli.am2@proton.me>
2026-08-11 11:03:50 +0900

    feat(browser): add zen-browser

--- flake.nix
+++ flake.nix
@@ -51,6 +51,11 @@
       url = "github:charmbracelet/nur";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    zen-browser = {
+      url = "github:youwen5/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
